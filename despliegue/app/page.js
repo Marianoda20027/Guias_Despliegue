496de5fc085a7852b2ebc63f81328978dce1e54f
Marianoda20027 <163364385+Marianoda20027@users.noreply.github.com>
2024-10-30 15:04:50 -0600

    Update page.js

--- despliegue/app/page.js
+++ despliegue/app/page.js
@@ -1,7 +1,36 @@
+import React from "react";
+
 export default function Home() {
   return (
-    <div>
-      <h1>Hola Mundo</h1>
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "100vh",
+        backgroundColor: "#f0f4f8",
+        margin: 0,
+      }}
+    >
+      <div
+        style={{
+          textAlign: "center",
+          backgroundColor: "#fff",
+          padding: "40px",
+          borderRadius: "10px",
+          boxShadow: "0 8px 16px rgba(0, 0, 0, 0.2)",
+        }}
+      >
+        <h1
+          style={{
+            fontSize: "2.5rem",
+            color: "#3a86ff",
+            marginBottom: "10px",
+          }}
+        >
+          Prueba Guía de Despliegue
+        </h1>
+      </div>
     </div>
   );
 }
